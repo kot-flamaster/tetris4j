90abc01ba1ee85712e6f51d6d24cff84d13971b5
kot-flamaster <kot.flamaster@gmail.com>
2023-03-18 03:58:05 +0200

    Display current figure

--- src/main/java/kot/flamaster/Board.java
+++ src/main/java/kot/flamaster/Board.java
@@ -3,9 +3,7 @@ package kot.flamaster;
 import kot.flamaster.logic.Tetromino;
 
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -17,6 +15,7 @@ public class Board extends JPanel {
     private static final int WIDTH = 10;
     private static final int HEIGHT = 20;
     private static final int CELL_SIZE = 30;
+    private static final int TILE_SIZE = 30;
 
     private Color[][] board;
 
@@ -24,6 +23,8 @@ public class Board extends JPanel {
     private Tetromino currentTetromino;
     private int currentX;
     private int currentY;
+
+    private int score;
 
     public Board() {
         board = new Color[WIDTH][HEIGHT];
@@ -33,6 +34,8 @@ public class Board extends JPanel {
         setFocusable(true);
         spawnNewTetromino();
         initTimer();
+
+        score = 0;
     }
 
     public void initKeyListener(){
@@ -58,6 +61,13 @@ public class Board extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        drawBoard(g);
+        drawTetromino(g);
+        drawInfo(g);
+
+    }
+
+    public void drawBoard(Graphics g){
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 g.setColor(board[x][y] != null ? board[x][y] : Color.BLACK);
@@ -67,6 +77,29 @@ public class Board extends JPanel {
             }
         }
     }
+
+    private void drawInfo(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.drawString("Score: " + score, 10, 20);
+        // Add more info if needed, e.g. level, lines cleared, etc.
+    }
+
+
+    private void drawTetromino(Graphics g) {
+        int[][] shape = currentTetromino.getShape();
+        for (int x = 0; x < shape.length; x++) {
+            for (int y = 0; y < shape[x].length; y++) {
+                if (shape[x][y] != 0) {
+                    g.setColor(currentTetromino.getColor());
+                    g.fillRect((currentX + x) * TILE_SIZE, (currentY + y) * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                    g.setColor(Color.BLACK);
+                    g.drawRect((currentX + x) * TILE_SIZE, (currentY + y) * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                }
+            }
+        }
+    }
+
     private boolean isValidMove(Tetromino tetromino, int[][] rotatedShape, int posX, int posY) {
         int[][] shape = tetromino.getShape();
 
